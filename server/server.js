1d90cf72d750510b3c319b60909dddb97ede9305
Besarta <73610437+bs47892@users.noreply.github.com>
2023-01-07 00:06:52 +0100

    Database connection POST and Get methods

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-//const mongoose = require("mongoose");
-//const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 //const cors = require("cors");
 
+const User = require ("./models/user");
+
+dotenv.config ();
+
 const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose.connect (
+    process.env.DATABASE,
+{useNewUrlParser: true, useUnifiedTopology: true}, 
+(err)=>{
+    if(err){
+        console.log(err);
+    }else {
+        console.log("Connected to Database");
+    }
+})
+
 
 // Middlewares
 //app.use(cors());
@@ -18,7 +35,19 @@ app.get ("/", (req,res)=>{
 });
 
 app.post ("/", (req, res)=>{
-    console.log(req.body.name);
+    let user = new User();
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.password=req.body.password;
+
+    user.save(err=> {
+        if(err){
+         res.json(err);
+        }else {
+            res.json ("Succesfully saved");
+        }
+    });
+    
 });
 app.listen(3000, err => {
     if (err) {
